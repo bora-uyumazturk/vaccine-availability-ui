b8e41c43470212de7b89b85f6dcf2c9693a5b122
bora-uyumazturk <bora.uyumazturk@gmail.com>
2021-03-13 13:44:15 -0500

    try different route

--- components/map.js
+++ components/map.js
@@ -148,7 +148,9 @@ export default function Map({ center, location, changeLocation, entries }) {
 
     // fetch gazetteer data
     const setData = async () => {
-      dataRef.current = await fetch("/api/gazetteer").then((res) => res.json());
+      dataRef.current = await fetch("/api/gazetteer/").then((res) =>
+        res.json()
+      );
       dataRef.current = dataRef.current.data;
     };
     setData();
