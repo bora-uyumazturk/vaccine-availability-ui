0cd1fab1c46794a9ed0fe6fc42b9fdecd54c6a70
bora-uyumazturk <bora.uyumazturk@gmail.com>
2021-03-14 23:45:30 -0400

    change mobile behavior

--- components/localeList.js
+++ components/localeList.js
@@ -3,6 +3,11 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { toIdentifier } from "../lib/utils";
 
+import resolveConfig from "tailwindcss/resolveConfig";
+import tailwindConfig from "../tailwind.config.js";
+
+const fullConfig = resolveConfig(tailwindConfig);
+
 export default function LocaleList({ location, changeLocation, entries }) {
   // https://www.robinwieruch.de/react-scroll-to-item
   const refs = entries.reduce((acc, value) => {
@@ -13,11 +18,16 @@ export default function LocaleList({ location, changeLocation, entries }) {
   const [clicked, setClicked] = useState(false);
 
   useEffect(() => {
-    if (location && !clicked) {
-      refs[location.toLowerCase()].current.scrollIntoView({
-        behavior: "smooth",
-        block: "center",
-      });
+    const small =
+      window.screen.width <=
+      parseFloat(fullConfig.theme.screens.md.slice(0, -2));
+    if (location) {
+      if (!clicked || small) {
+        refs[location.toLowerCase()].current.scrollIntoView({
+          behavior: small ? "auto" : "smooth",
+          block: "center",
+        });
+      }
     }
 
     setClicked(false);
@@ -26,7 +36,7 @@ export default function LocaleList({ location, changeLocation, entries }) {
   return (
     <div
       onClick={() => setClicked(true)}
-      className="flex flex-row md:flex-col border overflow-scroll h-32 min-h-min md:h-full md:max-h-full md:min-h-0 w-3/4 md:w-1/5 md:divide-y"
+      className="flex flex-row md:flex-col border overflow-hidden md:overflow-scroll h-32 min-h-min md:h-full md:max-h-full md:min-h-0 w-3/4 md:w-1/5 md:divide-y"
     >
       {entries.map(
         (e) =>
